a3b70a9a849bf16f0afa4edb434a5dfd261dcde0
Alexandre Iabrudi <alexandre.iabrudi@dtidigital.com.br>
2021-10-24 21:15:17 -0300

    ClienteApiModel criado ClienteController Criado

---
+++ TesteContaBancaria/ApiModel/ClienteApiModel.cs
@@ -0,0 +1,9 @@
+﻿namespace TesteContaBancaria.ApiModel
+{
+    public class ClienteApiModel
+    {
+        public string Nome { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+    }
+}
---
+++ TesteContaBancaria/Controllers/ClienteController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace TesteContaBancaria.Controllers
+{
+    [ApiController]
+    [Route("clientes")]
+    public class ClienteController : ControllerBase
+    {
+    }
+}
